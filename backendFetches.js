92b55f31226747e7910f581cb34acd0e0c6c4707
vya4eslavv <slavinidzecinema@yandex.ru>
2025-05-12 15:52:40 +0300

    fixed front cards

--- backendFetches.js
+++ backendFetches.js
@@ -1,18 +1,18 @@
-async function fetchData(url){
+    async function fetchData(url){
     let result = await fetch(url);
     return result.json()
 }
 
 
-fetchData("http://localhost:8080/api/invention/get/13").then(data => {
+    fetchData("http://localhost:8080/api/invention/get/13").then(data => {
     document.getElementById("map").innerHTML += data.html_code;
 })
 
 
-fetchData("http://localhost:8080/api/invention/get/2").then(data => {
+    fetchData("http://localhost:8080/api/invention/get/2").then(data => {
     document.getElementById("map").innerHTML += data.html_code;
 })
 
-fetchData("http://localhost:8080/api/invention/get/11").then(data => {
+    fetchData("http://localhost:8080/api/invention/get/11").then(data => {
     document.getElementById("map").innerHTML += data.html_code;
 })
